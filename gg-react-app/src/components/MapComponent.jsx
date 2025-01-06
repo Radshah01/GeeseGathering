0b454549286ca2cf81691e12f48a87b6190e65e0
Radshah01 <shahravanrs@gmail.com>
2025-01-06 17:12:23 -0500

    markers on map
    the display is having issues. i might add the styling in css rather than component

--- gg-react-app/src/components/MapComponent.jsx
+++ gg-react-app/src/components/MapComponent.jsx
@@ -1,11 +1,29 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+
+
 
 
 const MapComponent = () => {
     const mapContainerRef = useRef(null);
     const mapRef = useRef(null);
+    const [markers, setMarkers] = useState([]);
+
+    const markerRoomLocations = [
+        { lng: -79.2545, lat: 43.0093 },
+        { lng: -79.2475, lat: 43.0089 },
+    ];
+
+    const createCustomMarkerElement = () => {
+        const element = document.createElement('div');
+        element.className = 'custom-room-marker';
+        element.style.width = '200px';
+        element.style.height = '200px';
+        element.style.borderRadius = '50%';
+        element.style.backgroundColor = 'rgb(167, 103, 236)';
+        return element;
+    }
   
     useEffect(() => {
         mapboxgl.accessToken = 'pk.eyJ1IjoibGVtbW9yYWQiLCJhIjoiY201aXlxZzl3MDN6dDJ2cTJraW0yd3BzOCJ9.26qmYpFEIGdDTzukqoST6w';
@@ -17,15 +35,21 @@ const MapComponent = () => {
             zoom: 16.2,
             pitch: 65,
             bearing: 230,
-            antialias: true
+            antialias: true       
     });
 
+
+
     mapRef.current.on('load', () => {
+        const canvas = mapRef.current.getCanvas();
+        canvas.style.borderRadius = '36px';
+
+
         const layers = mapRef.current.getStyle().layers;
         const labelLayerId = layers.find(
             (layer) => layer.type === 'symbol' && layer.layout['text-field']
         ).id;
-
+        
 
 
     mapRef.current.addLayer({
@@ -60,8 +84,20 @@ const MapComponent = () => {
       },
       labelLayerId
     );
+
+    const newRoomMarkers = markerRoomLocations.map(roomLocation => {
+        const marker = new mapboxgl.Marker(createCustomMarkerElement())
+                                .setLngLat([roomLocation.lng, roomLocation.lat])
+                                .addTo(mapRef.current);
+        return marker
+    })
+
+    setMarkers(newRoomMarkers);
+
+    
     });
 
+   
 
     mapRef.current.on('style.load', () => {
         mapRef.current.setConfigProperty('basemap', 'lightPreset', 'dusk');
@@ -69,10 +105,17 @@ const MapComponent = () => {
         mapRef.current.setConfigProperty('basemap', 'show3dObjects', true);
         });
 
-  return () => mapRef.current.remove();
+  return () => {
+    markers.forEach(marker => marker.remove());
+    mapRef.current.remove();
+  }
 }, []);
   
-    return <div ref={mapContainerRef} style={{ height: '100%' }} />;
-  };
+    return (
+    <>  
+        <div ref={mapContainerRef} style={{ height: "100%" , borderRadius: '36px', overflow: 'hidden'}} />
+    </>
+    )
+  }
   
   export default MapComponent;

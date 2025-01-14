f55496138a4f35ad322f1cfb594e12421e466cad
Radshah01 <142700835+Radshah01@users.noreply.github.com>
2025-01-13 23:14:50 -0800

    decision changed, brought back Friends
    "under development"

--- gg-react-app/src/pages/Friends.jsx
+++ gg-react-app/src/pages/Friends.jsx
@@ -5,6 +5,9 @@ const Friends = () => {
     return(
         <>
          <Header />
+         <div style={{ display: "flex", justifyContent: "center", marginTop: "200px",marginBottom: "900px"}}>
+         <p style={{ fontSize: "2em", fontWeight: "bold"}}>This page is currently under development..</p>
+        </div>
         </>
     );
 }
